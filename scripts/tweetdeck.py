6ad00a31baf723a5bd8aa2565e17a00e2015bef7
Julian Metzler <git@mezgrman.de>
2014-05-21 19:08:00 +0200

    Some nonfunctional changes

--- scripts/tweetdeck.py
+++ scripts/tweetdeck.py
@@ -4,11 +4,22 @@
 import gtk
 import webkit
 import gobject
+import subprocess
+
+def open_link(view, frame, request, action, decision):
+	# WHY THE FUCK ISN'T THIS WORKING
+	uri = request.get_uri()
+	if not uri.startswith("https://tweetdeck.twitter.com"):
+		decision.ignore() # Tell WebKit to not give a fuck about the link, we handle this shit
+		subprocess.call(["xdg-open", uri]) # HANDLE IT MOTHERFUCKER
+	
+	return False
 
 def main():
 	gobject.threads_init()
 	win = gtk.Window()
 	bro = webkit.WebView()
+	bro.connect("navigation-policy-decision-requested", open_link)
 	bro.open("https://tweetdeck.twitter.com/")
 	win.add(bro)
 	win.show_all()
